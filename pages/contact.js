2f6c8c7d9e69d0dfb224ed1fc67157e86486c724
Luke Twomey <luke.twomey85@gmail.com>
2023-01-27 10:20:59 +0000

    Add Contact page

---
+++ pages/contact.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Head from "next/head";
+
+export const Contact = () => {
+  return (
+    <div>
+      <Head>
+        <meta charset="utf-8" />
+        <meta name="author" content="Luke Twomey" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Contact</title>
+        <meta
+          name="description"
+          content="Content page description here..."
+        ></meta>
+      </Head>
+      <main>
+        <h1>Contact</h1>
+        <p>Lorem ipsum and all that jazz...</p>
+        <p>A whole big list of great projects to show off</p>
+        <p>
+          Five genuinely enjoyable years later, I jumped at the opportunity to
+          explore more of the world, backpacking for a year. Aside from
+          countless incredible experiences, this gave me ample opportunity to
+          choose an exciting new career path.
+        </p>
+        <p>
+          Five genuinely enjoyable years later, I jumped at the opportunity to
+          explore more of the world, backpacking for a year. Aside from
+          countless incredible experiences, this gave me ample opportunity to
+          choose an exciting new career path.
+        </p>
+        <p>
+          Five genuinely enjoyable years later, I jumped at the opportunity to
+          explore more of the world, backpacking for a year. Aside from
+          countless incredible experiences, this gave me ample opportunity to
+          choose an exciting new career path.
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default Contact;
