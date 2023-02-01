8fa092699458460497546474cb4f1f2e87a07b28
Luke Twomey <luke.twomey85@gmail.com>
2023-02-01 10:24:24 +0000

    Load fonts server-side so client-side google network request is unnecessary

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,29 @@
 import React from "react";
 import App from "next/app";
+import { Titillium_Web } from "@next/font/google";
 import Layout from "../components/Layout";
 import "../styles/global.css";
+
+const titillium = Titillium_Web({
+  weight: ["200", "300", "400", "600", "700", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <>
+        <style jsx global>{`
+          html {
+            font-family: ${titillium.style.fontFamily};
+          }
+        `}</style>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </>
     );
   }
 }
--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,6 @@ export default function Document() {
     <Html>
       <Head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="images/favicon.ico" />
-        <meta name="theme-color" content="#000000" />
         <meta name="description" content="Here's the meta description" />
         <link
           rel="apple-touch-icon"
@@ -29,14 +27,6 @@ export default function Document() {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
-
-        {/* <link rel="manifest" href="/manifest.json" /> */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Asap:wght@300&family=Bebas+Neue&family=Chivo:wght@300&family=Electrolize&family=PT+Mono&family=Poiret+One&family=Rajdhani:wght@500&family=Saira+Condensed&family=Saira:wght@200;300;400&family=Signika&family=Titillium+Web&display=swap"
-          rel="stylesheet"
-        />
         <script
           async
           data-uid="93649111fd"
