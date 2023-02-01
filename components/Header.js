bc049082f631d8591638d944c386e06d5e45fc21
Luke Twomey <luke.twomey85@gmail.com>
2023-02-01 08:00:18 +0000

    Add new logo

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React, { useState } from "react";
 import { Nav } from "./Nav";
 import styles from "./Header.module.css";
@@ -21,6 +22,14 @@ export const Header = () => {
   return (
     <header className={navOpen ? "navOpen" : ""}>
       <section className={styles.title}>
+        <Image
+          src="/images/logo.webp"
+          alt="Luke the Web Dev Logo"
+          width="100"
+          height="100"
+          priority
+          className={styles.logo}
+        />
         <a href="/" onClick={closeNav}>
           <h1>Luke the Web Dev</h1>
         </a>
