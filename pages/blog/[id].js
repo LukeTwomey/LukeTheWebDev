225c28855251d7549f2fe804948ec2fd238db3e1
Luke Twomey <luke.twomey85@gmail.com>
2023-02-03 07:27:16 +0000

    Strip right back to nothing on the page

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -20,61 +20,7 @@ const Post = ({ frontmatter, content, otherPosts }) => {
         <title>{frontmatter.title}</title>
         <meta name="description" content="Here's my great about page!"></meta>
       </Head>
-      <main className="blogPost">
-        <ReactMarkdown
-          remarkPlugins={[remarkGfm]}
-          linkTarget="_blank"
-          components={{
-            code({ node, inline, className, children, ...props }) {
-              return !inline ? (
-                <SyntaxHighlighter
-                  style={nord}
-                  language="javascript"
-                  PreTag="div"
-                  {...props}
-                >
-                  {String(children).replace(/\n$/, "")}
-                </SyntaxHighlighter>
-              ) : (
-                <code className="md-post-code" {...props}>
-                  {children}
-                </code>
-              );
-            },
-            h6({ node, inline, className, children, ...props }) {
-              return (
-                <Signup
-                  message="If you're enjoying the read, please consider signing up to my newsletter to receive notifications when new posts are added!"
-                  location="middleOfPost"
-                />
-              );
-            },
-          }}
-        >
-          {content}
-        </ReactMarkdown>
-
-        <Signup
-          message="If you enjoyed the read, please consider signing up to my newsletter to receive notifications when new posts are added!"
-          location="endOfPost"
-        />
-
-        <Giscus
-          id="giscusComments"
-          repo="LukeTwomey/LukeTheWebDev"
-          repoId="R_kgDOI2bJWw"
-          category="Announcements"
-          categoryId="DIC_kwDOI2bJW84CT4Cv"
-          mapping="title"
-          term="Welcome to @giscus/react component!"
-          reactionsEnabled="1"
-          emitMetadata="0"
-          inputPosition="top"
-          theme="https://lukethewebdev.api.up.railway.app/giscusCss"
-          lang="en"
-          loading="lazy"
-        />
-      </main>
+      <h1>Well this is sad</h1>
     </div>
   );
 };
