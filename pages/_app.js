9a89b3e60539cf2d3cf3a5cc21bf5715222ca5fb
Luke Twomey <luke.twomey85@gmail.com>
2023-02-06 19:56:14 +0000

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Script from "next/script";
 import App from "next/app";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/ga/gtag";
 import { Titillium_Web } from "@next/font/google";
 import Layout from "../components/Layout";
 import "../styles/global.css";
@@ -10,20 +13,46 @@ const titillium = Titillium_Web({
   subsets: ["latin"],
 });
 
-export default class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <>
-        <style jsx global>{`
-          html {
-            font-family: ${titillium.style.fontFamily};
-          }
-        `}</style>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </>
-    );
-  }
+export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <style jsx global>{`
+        html {
+          font-family: ${titillium.style.fontFamily};
+        }
+      `}</style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
